4fc975b57d5a99daf4e331751c12e593e076f1ac
abigailCul <G00334291@gmit.ie>
2017-11-14 11:25:23 +0000

    Serve Html

--- ajax.go
+++ ajax.go
@@ -3,7 +3,20 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"html/template"
 )
+type Chat struct{
+	Message string
+}
+func templatehandler(w http.ResponseWriter, r *http.Request){
+
+	m := Chat{Message: "Eliza"}
+
+	t, _ := template.ParseFiles("chatPage.html")
+
+	t.Execute(w, &m)
+
+}
 
 func userinputhandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello, %s!", r.URL.Query().Get("value")) //.Path[1:])
@@ -12,9 +25,10 @@ func userinputhandler(w http.ResponseWriter, r *http.Request) {
 func main() {
 
 	// Adapted from: http://www.alexedwards.net/blog/serving-static-sites-with-go
-	fs := http.FileServer(http.Dir("static"))
-	http.Handle("/", fs)
+	//fs:=  http.FileServer(http.Dir("chatPage"))
+	//http.HandleFunc("/", userinputhandler)
+	http.HandleFunc("/", templatehandler)
 
-	http.HandleFunc("/user-input", userinputhandler)
+	//http.HandleFunc("/chatPage", userinputhandler)
 	http.ListenAndServe(":8080", nil)
 }
